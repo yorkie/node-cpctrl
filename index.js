fe9b14ad8eeba8c565761d5cfb0a7c749735dfb5
Yazhong Liu <yorkiefixer@gmail.com>
2014-08-20 01:26:25 +0800

    fix

--- index.js
+++ index.js
@@ -7,12 +7,16 @@ function stop(child) {
   if (!child || !child.kill || !child.send)
     throw new Error('cannot stop itself or non-ChildProcess object');
   child.kill(stop);
+  child.stoped = true;
 }
 
 function resume(child) {
   if (!child || !child.kill || !child.send)
     throw new Error('cannot stop itself or non-ChildProcess object');
+  if (!child.stoped)
+    throw new Error('you just resume that stoped process');
   child.kill(cont);
+  child.stoped = false;
 }
 
 exports.stop = stop;
